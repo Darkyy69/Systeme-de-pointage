f91d958c0dd85dd2a5b616b60240a79ff9333147
Darkyy69 <radiasweetgirl812@gmail.com>
2024-02-13 02:54:58 +0100

    removed .py files

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-if matricule == search_query:
-            #     # Select the row in the Treeview
-            #     employees_treeview.selection_add(item_id)
-            #     # Scroll to the selected row
-            #     employees_treeview.see(item_id)
-            #     break  # Stop searching after finding the first match
